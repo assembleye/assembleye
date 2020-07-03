6fc14b5fe42fa431c712a1991abca32db223998b
Gnlow <entrydark@gmail.com>
2020-07-03 23:29:14 +0900

    draw circle

--- src/chart.tsx
+++ src/chart.tsx
@@ -13,9 +13,10 @@ const Canvas = props => {
     </canvas>
 }
 const Seat = ({canvasRef}: {canvasRef: MutableRefObject<HTMLCanvasElement>}) => {
-    usePaint(canvasRef, context => {
-        context.fillStyle = "#000000"
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+    usePaint(canvasRef, ctx => {
+        ctx.beginPath()
+        ctx.arc(ctx.canvas.width / 2, ctx.canvas.height / 2, 50, 0, Math.PI * 2)
+        ctx.fill()
     })
     return null
 }
