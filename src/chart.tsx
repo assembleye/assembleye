8c6719b993a50bfce6210ab7d323822c1fc0d22d
Gnlow <entrydark@gmail.com>
2020-07-03 10:28:05 +0900

    canvas component test

--- src/chart.tsx
+++ src/chart.tsx
@@ -1,7 +1,26 @@
-import * as React from "react"
+import React, {
+    useRef,
+    useEffect,
+    MutableRefObject,
+} from "react"
 import {render} from "react-dom"
 
+const Canvas = props => {
+    const canvasRef = useRef(null)    
+    return <canvas ref={canvasRef} {...props}>
+        <Seat canvasRef={canvasRef} />
+    </canvas>
+}
+const Seat = ({canvasRef}: {canvasRef: MutableRefObject<HTMLCanvasElement>}) => {
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext("2d")
+        context.fillStyle = "#000000"
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+    })
+    return <div></div>
+}
 render(
-    <h1>Hello, World!</h1>,
+    <Canvas width="150" height="150"></Canvas>,
     document.getElementById("app")
 )
